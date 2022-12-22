f18faa589f901b29a472363a0e17eea2cf38da2f
jarvisar <jarvisar@mail.uc.edu>
2022-12-22 17:13:53 -0500

    Finally added HTML page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log('Using limit: ', myLimit);
 
 app.use(bodyParser.json({limit: myLimit}));
 
-app.all('*', function (req, res, next) {
+app.all('/proxy/TAP/sync', function (req, res, next) {
 
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "https://jarvisar.github.io");
@@ -22,20 +22,24 @@ app.all('*', function (req, res, next) {
         var targetURL = req.header('Target-URL');
         if (!targetURL) {
             res.writeHead(200, {
-                'Content-Type': 'text'
+                'Content-Type': 'text/html'
             });
-            res.write('CORS-Proxy')
+            res.write(req.url)
             res.end()
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body },
+        request({ url: targetURL + req.url.replace('/proxy/', ''), method: req.method, json: req.body },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
                 }
-//                console.log(body);
+                console.log(body);
             }).pipe(res);
     }
+});
+
+app.get('/', function(req, res){
+    res.sendfile('./index.html')
 });
 
 app.set('port', process.env.PORT || 3000);
