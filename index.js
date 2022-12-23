877c5960c6a3029e7c76fd1d832b941545428887
jarvisar <jarvisar@mail.uc.edu>
2022-12-22 22:07:14 -0500

    Testing new HTML and CSS

--- index.js
+++ index.js
@@ -10,32 +10,23 @@ app.use(bodyParser.json({limit: myLimit}));
 
 app.all('/proxy/TAP/sync', function (req, res, next) {
 
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    // Manually set CORS headers
     res.header("Access-Control-Allow-Origin", "https://jarvisar.github.io");
     res.header("Access-Control-Allow-Methods", "GET");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-    let fs = require('fs');
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.send();
-    } else {
-        var targetURL = req.header('Target-URL');
-        if (!targetURL) {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write(req.url)
-            res.end()
-            return;
-        }
-        request({ url: targetURL + req.url.replace('/proxy/', ''), method: req.method, json: req.body },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-                console.log(body);
-            }).pipe(res);
+
+    var targetURL = req.header('Target-URL');
+    if (!targetURL) {
+        res.send(500, { error: 'Target-URL header is missing from the request.'})
+        return;
     }
+    request({ url: targetURL + req.url.replace('/proxy/', ''), method: req.method, json: req.body },
+        function (error, response, body) {
+            if (error) {
+                console.error('error: ' + response.statusCode)
+            }
+            console.log(body);
+        }).pipe(res);
 });
 
 app.get('/', function(req, res){
