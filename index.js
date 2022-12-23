b7a437bc1e7be56eb9fb197a3c8af00470fb35a3
jarvisar <jarvisar@mail.uc.edu>
2022-12-23 12:47:30 -0500

    PReflight

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.all('*', function (req, res, next) {
 
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
-        res.send();
+        res.send(http.statusOk);
     } else {
         var targetURL = req.header('Target-URL');
         if (!targetURL) {
