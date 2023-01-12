24af3f0a9fa9403576f6ece5f6432eb9fc9201c7
jarvisar <jarvisar@mail.uc.edu>
2023-01-12 15:53:19 -0500

    Updated text and added comments

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+// Express.js
 var express = require('express'),
     request = require('request'),
     bodyParser = require('body-parser'),
     app = express();
 
+// Read parameters from command line
 const port = process.argv[2];
 const defaultURL = process.argv[3];
 
 app.all('/proxy/TAP/sync', function (req, res, next) {
 
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    // Set headers here. Allows all methods from all origins by default.
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
+    // Pre-flight
     if (req.method === 'OPTIONS') {
-        // CORS Preflight
         res.send();
     } else {
         var targetURL = req.header('Target-URL');
+        // If no Target-URL, check if default URL was provided as parameter
         if (!targetURL) {
             if (defaultURL == undefined) {
-                res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+                res.send(500, { error: 'There is no Target-URL header in the request' });
                 return;
             }
             else {
@@ -48,5 +51,5 @@ app.get('/', function(req, res) {
 app.set('port', process.env.PORT || port || 3000);
 
 app.listen(app.get('port'), function () {
-    console.log('Proxy server listening on port ' + app.get('port'));
+    console.log('CORS Proxy server listening on port ' + app.get('port'));
 });
